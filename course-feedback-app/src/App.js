5055ac844cdffddd39e450d63ba5189408a4f90f
Sarah Allerheiligen <sarahallerheiligen@Sarahs-MBP-9.quinnipiac.edu>
2024-12-02 15:26:38 -0500

    created bar graphs

--- course-feedback-app/src/App.js
+++ course-feedback-app/src/App.js
@@ -2,29 +2,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import { CourseList } from "./components/CourseList";
 import { LessonList } from "./components/LessonList";
+import { Bargraph1 } from "./components/EasyBar";
+import { Bargraph2 } from "./components/PaceBar";
 
 function App() {
   return (
     //This is the CourseList element.
 
-    <div className="Container">
+    <div className="App">
       <div className="row">
-        <div className="col-md-1"></div>
-        <div className="col-md-10">
-          <CourseList></CourseList>
+        <div className="col-md-6">
+          <Bargraph1 />
         </div>
-        <div className="col-md-1"></div>
-      </div>
-
-      {/* this is what loads in the lessonList */}
-      <div className="row">
-        <div className="col-md-1"></div>
-        <div className="col-md-10">
-          <LessonList></LessonList>
+        <div className="col-md-6">
+          <Bargraph2 />
         </div>
-        <div className="col-md-1"></div>
       </div>
     </div>
+
+    // <div className="Container">
+    //   <div className="row">
+    //     <div className="col-md-1"></div>
+    //     <div className="col-md-10">
+    //       <CourseList></CourseList>
+    //     </div>
+    //     <div className="col-md-1"></div>
+    //   </div>
+
+    //   {/* this is what loads in the lessonList */}
+    //   <div className="row">
+    //     <div className="col-md-1"></div>
+    //     <div className="col-md-10">
+    //       <LessonList></LessonList>
+    //     </div>
+    //     <div className="col-md-1"></div>
+    //   </div>
+    // </div>
   );
 }
 
