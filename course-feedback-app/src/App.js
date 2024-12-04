0370014b5a195ed0cd5623f7df69513ccf39473b
gabbypierce <gab965@att.net>
2024-12-03 19:32:31 -0500

    feedback pages

--- course-feedback-app/src/App.js
+++ course-feedback-app/src/App.js
@@ -3,21 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import { CourseList } from "./components/CourseList";
 import { LessonList } from "./components/LessonList";
+import FeedbackPage from "./components/FeedbackPage";
 import LoginForm from "./components//LoginForm";
 import EditProfileStudent from "./components/EditProfileStudent";
 import EditProfileProf from "./components/EditProfileProf";
 import { Bargraph1 } from "./components/EasyBar";
 import { Bargraph2 } from "./components/PaceBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TeacherFeedbackPage from "./components/TeacherFeedbackPage";
 
+/* 
+ function App() { 
+  return ( 
+    <div> <TeacherFeedbackPage /> </div> 
+    );
+   } 
+*/ 
 
 function App() {
   return (
-   /* <div className="App">
-    {/* Render the EditProfile component }
-    <CourseList/>
-  </div>); */
-   <Router>
+    /* <div className="App"> 
+    {/* Render the EditProfile component } 
+    <CourseList/> </div>); */
+
+    <Router>
       <Routes>
         {/* Default Login Page */}
         <Route path="/" element={<LoginForm />} />
@@ -33,9 +42,13 @@ function App() {
 
         {/* Lesson List Page */}
         <Route path="/lesson-list" element={<LessonList />} />
+
+        <Route path="/lessons" element={<LessonList />} />
+        <Route path="/feedback" element={<TeacherFeedbackPage />} />
+        <Route path="/feedback/:lessonId" element={<FeedbackPage />} />
       </Routes>
     </Router>
-      
+
   );
 }
 
