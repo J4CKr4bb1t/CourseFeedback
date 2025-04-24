7734ad6e3113a72380f6f5cac15bb31e6347a860
charlizeaponte <charlizeaponte@yahoo.com>
2025-04-24 11:29:02 -0400

    Added app.js

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express()
+const authRouter = require('./routes/authRouter');
+const classRouter = require('./routes/classRouter');
+const feedbackRouter = require('./routes/feedbackRouter');
+const lessonRouter = require('./routes/lessonRouter');
 
-var authRouter = require('./routes/authRouter');
-var classRouter = require('./routes/classRouter');
-var feedbackRouter = require('./routes/feedbackRouter');
-var lessonRouter = require('./routes/lessonRouter');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(morgan('dev'));
+app.use(bodyParser.json()); 
 app.use('/', authRouter);
 app.use('/class', classRouter);
 app.use('/feedback', feedbackRouter);
 app.use('/lesson',lessonRouter)
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+app.use((err, req, res, next) => {
+  console.error(err); 
+  res.status(500).json({ error: 'Internal Server Error' }); 
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(3000, () => {
+console.log(`Server running at http://localhost:${3000}/`);
 });
-
-module.exports = app;
